d9befb16bcfd6be293ec0a6e7f8105973b75cedd
Harsh-Inder <87305157+Harsh-Inder@users.noreply.github.com>
2022-11-16 23:38:19 +0530

    Add files via upload

---
+++ front-end/src/components/carousel.js
@@ -0,0 +1,43 @@
+const Carousel = ()=>(
+    <div id="carouselExampleDark" class="carousel carousel-dark slide mb-5" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"></li>
+      <li data-bs-target="#carouselExampleDark" data-bs-slide-to="1"></li>
+      <li data-bs-target="#carouselExampleDark" data-bs-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="2500">
+        <img src="images/product-2-3.jpg" class="d-block w-100" alt="..."/>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Buy Smart watch now</h5>
+          <p><b>10% </b>DISCOUNT ON EVERY ITEM</p>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2500">
+        <img src="images/product-3-slider.jpg" class="d-block w-100" alt="..."/>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Get the latest iPhones now</h5>
+          <p>Available in various colors</p>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2500">
+        <img src="images/product-6-2.jpg" class="d-block w-100" alt="..."/>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Click to buy drones</h5>
+          <p>Get delivery within 5 days across India</p>
+        </div>
+      </div>
+      
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleDark" role="button" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleDark" role="button" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </a>
+  </div>
+)
+
+export default Carousel;
